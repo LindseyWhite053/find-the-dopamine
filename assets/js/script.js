adf1ebf3425d7394f0ba9ae04db3aab88932745c
Lindsey White <LindseyWhite053@Gmail.com>
2022-05-11 20:06:40 -0400

    fetched API data

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var jokeFind = function() {
+    fetch('https://v2.jokeapi.dev/joke/Any')
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+//documentation found at https://sv443.net/jokeapi/v2/
+
+jokeFind();
+
+var memeFind = function() {
+    fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+// documentation found at https://imgflip.com/api
+
+memeFind();
+
